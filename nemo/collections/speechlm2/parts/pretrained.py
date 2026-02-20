c5d13d449b7cc7279e23e66a2f9c2f21890ea0db
Desh Raj <r.desh26@gmail.com>
2026-02-20 13:23:22 -0800

    Fix import issues

--- nemo/collections/speechlm2/parts/pretrained.py
+++ nemo/collections/speechlm2/parts/pretrained.py
@@ -73,9 +73,9 @@ def load_pretrained_automodel(model_path_or_name: str, pretrained_weights: bool 
     from nemo_automodel import NeMoAutoModelForCausalLM
 
     if pretrained_weights:
-        return NeMoAutoModelForCausalLM.from_pretrained(model_path_or_name, torch_dtype=dtype, **kwargs)
+        return NeMoAutoModelForCausalLM.from_pretrained(model_path_or_name, torch_dtype=dtype, trust_remote_code=True, **kwargs)
     else:
-        config = AutoConfig.from_pretrained(model_path_or_name)
+        config = AutoConfig.from_pretrained(model_path_or_name, trust_remote_code=True)
         return NeMoAutoModelForCausalLM.from_config(config, torch_dtype=dtype, **kwargs)
 
 
